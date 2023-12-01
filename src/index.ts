c3c21808f76d62308e9f491b965236521e23cbb0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-01 03:53:45 +0000

    update bundle

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-import * as core from "@actions/core";
+import * as core from '@actions/core'
 
-import { attemptInstall } from "./installation/installation";
+import { attemptInstall } from './installation/installation'
 
-
-async function main()
-{
-    const info = await attemptInstall();
-    core.setOutput('directory', info.directory);
-    core.setOutput('executable', info.executable);
-    core.addPath(info.binDirectory);
+async function main() {
+  const info = await attemptInstall()
+  core.setOutput('directory', info.directory)
+  core.setOutput('executable', info.executable)
+  core.addPath(info.binDirectory)
 }
 
 async function wrap_main() {
-    await main()
-        .catch(error => core.setFailed(error));
+  await main().catch(error => core.setFailed(error))
 }
 
 void wrap_main()
